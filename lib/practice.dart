5ff09f2aef68d002cc2b7bfd71068c24a96dd661
Amangupt123 <gaman2882@gmail.com>
2022-05-25 12:17:10 +0530

    everything

--- lib/practice.dart
+++ lib/practice.dart
@@ -16,9 +16,9 @@ class _practiceState extends State<practice> {
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text("Practice Quiz"),
+          title: const Text("Practice Quiz"),
           toolbarHeight: 100,
-          backgroundColor: Color(0xffF8922D),
+          backgroundColor: const Color(0xffF8922D),
           leadingWidth: 42,
           leading: Padding(
             padding: const EdgeInsets.only(left: 12.0),
@@ -33,42 +33,60 @@ class _practiceState extends State<practice> {
             ),
           ),
         ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Container(
-              margin: EdgeInsets.all(8),
-              child: Container(
+        body: Padding(
+          padding: const EdgeInsets.only(
+            top: 16.0,
+            left: 16,
+            right: 16,
+          ),
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (
+              context,
+              index,
+            ) {
+              return Container(
+                margin: EdgeInsets.only(bottom: 15),
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white),
-                child: ListTile(
-                    //Color:Colors.black,
-                    // leading: Icon(Icons.message),.
+                    borderRadius: BorderRadius.circular(0),
+                    color: Colors.red,
+                    border: Border(
+                      left: BorderSide(color: Color(0xffFC942D), width: 6),
+                      right: BorderSide(width: 1, color: Colors.grey),
+                      top: BorderSide(width: 1, color: Colors.grey),
+                      bottom: BorderSide(width: 1, color: Colors.grey),
+                    )),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: ListTile(
+                      // radius: 28,
+                      //Color:Colors.black,
+                      // leading: Icon(Icons.message),.
 
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      //   crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          'Test ${index + 1}',
-                          style: TextStyle(
-                              color: Color(
-                                0xff111128,
-                              ),
-                              fontWeight: FontWeight.w700),
-                        ),
-                        Image.asset(
-                          "assets/play.png",
-                          height: 33,
-                          width: 33,
-                        )
-                      ],
-                    ),
-                    subtitle: Text("Date ${index + 17} Apr 2022")),
-              ),
-            );
-          },
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        //   crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Test ${index + 1}',
+                            style: const TextStyle(
+                                color: const Color(
+                                  0xff111128,
+                                ),
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Image.asset(
+                            "assets/play.png",
+                            height: 33,
+                            width: 33,
+                          )
+                        ],
+                      ),
+                      subtitle: Text("Date ${index + 17} Apr 2022")),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
